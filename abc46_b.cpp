f4e5fe1a7fc189a59d227005ee3dfe2be7b8636c
HaraguchiMasahiro <momochan5454@gmail.com>
2019-11-23 17:59:36 +0900

    11/23

---
+++ abc46_b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+const long double PI = (acos(-1));
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+int ctoi(char c)
+{
+    switch (c)
+    {
+    case '0':
+        return 0;
+    case '1':
+        return 1;
+    case '2':
+        return 2;
+    case '3':
+        return 3;
+    case '4':
+        return 4;
+    case '5':
+        return 5;
+    case '6':
+        return 6;
+    case '7':
+        return 7;
+    case '8':
+        return 8;
+    case '9':
+        return 9;
+    default:
+        return 0;
+    }
+}
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int ans = k;
+    for (int i = 0; i < n - 1; i++)
+    {
+        ans *= (k - 1);
+    }
+    cout << ans << endl;
+}
