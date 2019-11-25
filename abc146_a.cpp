d60172c9f985479c3cb7e053c4485878bfbb6eba
HaraguchiMasahiro <momochan5454@gmail.com>
2019-11-25 22:06:03 +0900

    11/24

---
+++ abc146_a.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+const long double PI = (acos(-1));
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+int ctoi(char c)
+{
+    switch (c)
+    {
+    case '0':
+        return 0;
+    case '1':
+        return 1;
+    case '2':
+        return 2;
+    case '3':
+        return 3;
+    case '4':
+        return 4;
+    case '5':
+        return 5;
+    case '6':
+        return 6;
+    case '7':
+        return 7;
+    case '8':
+        return 8;
+    case '9':
+        return 9;
+    default:
+        return 0;
+    }
+}
+int main()
+{
+    string s;
+    cin >> s;
+    if (s == "SUN")
+    {
+        cout << 7 << endl;
+    }
+    else if (s == "MON")
+    {
+        cout << 6 << endl;
+    }
+    else if (s == "TUE")
+    {
+        cout << 5 << endl;
+    }
+    else if (s == "WED")
+    {
+        cout << 4 << endl;
+    }
+    else if (s == "THU")
+    {
+        cout << 3 << endl;
+    }
+    else if (s == "FRI")
+    {
+        cout << 2 << endl;
+    }
+    else
+    {
+        cout << 1 << endl;
+    }
+}
---
+++ abc146_b.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+const long double PI = (acos(-1));
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+int ctoi(char c)
+{
+    switch (c)
+    {
+    case '0':
+        return 0;
+    case '1':
+        return 1;
+    case '2':
+        return 2;
+    case '3':
+        return 3;
+    case '4':
+        return 4;
+    case '5':
+        return 5;
+    case '6':
+        return 6;
+    case '7':
+        return 7;
+    case '8':
+        return 8;
+    case '9':
+        return 9;
+    default:
+        return 0;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    char cnt = 'A';
+    for (int i = 0; i < 26; i++)
+    {
+        mp[i] = cnt;
+        cnt++;
+    }
+    map<int, char> pm;
+    int tnc = 0;
+    for (char i = 'A'; i <= 'Z'; i++)
+    {
+        pm[i] = tnc;
+        tnc++;
+    }
+    for (auto &x : s)
+    {
+        int j = pm[x];
+        j = (j + n) % 26;
+        x = mp[j];
+        cout << x;
+    }
+    cout << endl;
+}
