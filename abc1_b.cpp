c64d5c0c9cc3af09c9c15a6964b57bfcdde9b276
HaraguchiMasahiro <momochan5454@gmail.com>
2019-11-26 19:29:11 +0900

    11/26

---
+++ abc1_b.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+const long double PI = (acos(-1));
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+int ctoi(char c)
+{
+    switch (c)
+    {
+    case '0':
+        return 0;
+    case '1':
+        return 1;
+    case '2':
+        return 2;
+    case '3':
+        return 3;
+    case '4':
+        return 4;
+    case '5':
+        return 5;
+    case '6':
+        return 6;
+    case '7':
+        return 7;
+    case '8':
+        return 8;
+    case '9':
+        return 9;
+    default:
+        return 0;
+    }
+}
+int main()
+{
+    int m;
+    cin >> m;
+    if (m < 100)
+    {
+        m = 0;
+    }
+    else if (100 <= m && m <= 5000)
+    {
+        m /= 100;
+    }
+    else if (6000 <= m && m <= 30000)
+    {
+        m = m / 1000 + 50;
+    }
+    else if (35000 <= m && m <= 70000)
+    {
+        m = (m / 1000 - 30) / 5 + 80;
+    }
+    else if (m > 70000)
+    {
+        m = 89;
+    }
+    cout << setw(2) << setfill('0') << m << endl;
+}
