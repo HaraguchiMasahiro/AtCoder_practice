b7d781f801c38a51f1821ae919015bb3626b74dc
HaraguchiMasahiro <momochan5454@gmail.com>
2019-11-29 01:31:04 +0900

    11/29

---
+++ abc8_b.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+const long double PI = (acos(-1));
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+int ctoi(char c)
+{
+    switch (c)
+    {
+    case '0':
+        return 0;
+    case '1':
+        return 1;
+    case '2':
+        return 2;
+    case '3':
+        return 3;
+    case '4':
+        return 4;
+    case '5':
+        return 5;
+    case '6':
+        return 6;
+    case '7':
+        return 7;
+    case '8':
+        return 8;
+    case '9':
+        return 9;
+    default:
+        return 0;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    map<string, int> mp;
+    rep(i, n)
+    {
+        string s;
+        cin >> s;
+        mp[s]++;
+    }
+    string name;
+    int number = 0;
+    for (auto i : mp)
+    {
+        if (i.second > number)
+        {
+            number = i.second;
+            name = i.first;
+        }
+    }
+    cout << name << endl;
+}
