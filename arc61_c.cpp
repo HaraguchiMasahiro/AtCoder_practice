c7a4549e14f26281851204ae7167ca4575dae213
HaraguchiMasahiro <momochan5454@gmail.com>
2019-12-08 22:20:56 +0900

    12/8

---
+++ arc61_c.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+const long double PI = (acos(-1));
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+int ctoi(char c)
+{
+    switch (c)
+    {
+    case '0':
+        return 0;
+    case '1':
+        return 1;
+    case '2':
+        return 2;
+    case '3':
+        return 3;
+    case '4':
+        return 4;
+    case '5':
+        return 5;
+    case '6':
+        return 6;
+    case '7':
+        return 7;
+    case '8':
+        return 8;
+    case '9':
+        return 9;
+    default:
+        return 0;
+    }
+}
+int main()
+{
+    string s;
+    cin >> s;
+    int n = s.size();
+    ll k = (1 << (n - 1));
+    cout << k << endl;
+}
